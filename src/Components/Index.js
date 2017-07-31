edf0741d7bf6bb390f8f13299c94192c83be7619
suzhenhui <362680581@qq.com>
2017-07-31 15:15:32 +0800

    #新增react配置

---
+++ src/Components/Index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import Header from '@/Components/Header'
+
+export default class Index extends Component {
+    render() {
+        return (
+            <div id="app">
+                <Header/>
+                <div>123</div>
+            </div>
+        )
+    }
+}
